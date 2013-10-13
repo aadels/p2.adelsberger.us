d25a6163a3f042e9c673cb732db2a42f04468dc0
ann adelsberger <adelsbergerann@gmail.com>
2013-10-13 13:48:00 -0400

    moved p2 docs back

---
+++ README.md
@@ -0,0 +1,4 @@
+p2.adelsberger.us
+=================
+
+Project 2
---
+++ controllers/c_practice.php
@@ -0,0 +1,20 @@
+<?php{
+class practice_controller extends base_controller{
+	
+	public function test1(){
+	echo APP_PATH."<br>";
+	echo DOC_ROOT."<br>";
+
+	$imageObj = new Image('http://web.mit.edu/aadels/Public/appeal_images/_DSC0368.jpg/311,466')
+
+	$imageObj ->resize(155,233);
+
+	$imageObj-> display();
+
+}
+public function test2(){
+	echo Time::now();
+
+}
+
+}
