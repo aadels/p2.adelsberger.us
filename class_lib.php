22e5e41bf5629e56a1e994cce331d8e9710001d2
ann adelsberger <adelsbergerann@gmail.com>
2013-10-13 11:21:16 -0400

    initiated destructor

--- class_lib.php
+++ class_lib.php
@@ -7,10 +7,17 @@ class MyClass{
 
 		echo 'The class "', __CLASS__,'" was initiated!<br ?>';
 	}
+	
+	public function __destruct(){
 
+		echo 'The class "', __CLASS__.'" was destroyed.<br />';
+	}
+	
 	public function setProperty($newval){
+		
 		$this->prop1 = $newval;
 	}
+	
 	public function getProperty(){
 
 		return $this->prop1 . "<br />";
