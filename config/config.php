1a717218c59607cdc7928b31a27084a38410e0fb
ann adelsberger <adelsbergerann@gmail.com>
2013-10-14 06:35:50 -0400

    changed database name

--- config/config.php
+++ config/config.php
@@ -31,7 +31,7 @@ A email designated to receive messages from the server. Examples:
 	define('SYSTEM_EMAIL', 'adelsbergerann@gmail.com'); 
 
 # Default DB name for this app
-	define('DB_NAME', "p2_adelsberger.us"); 
+	define('DB_NAME', "p2_adelsberger_us"); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
