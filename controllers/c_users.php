60749db4366803d16e3e0a5cf1e6f3a690e0e50b
ann adelsberger <adelsbergerann@gmail.com>
2013-10-16 17:21:45 -0400

    removes code for signup

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -49,27 +49,6 @@ class users_controller extends base_controller {
 
     }
 
-    public function __construct() {
-        parent::__construct();
-    } 
-
-    public function signup() {
-
-        # Setup view
-            $this->template->content = View::instance('v_users_signup');
-            $this->template->title   = "Sign Up";
-
-        # Render template
-            echo $this->template;
-
-    }
-
-    public function p_signup() {
-
-        # Dump out the results of POST to see what the form submitted
-        echo '<pre>';
-        print_r($_POST);
-        echo '</pre>';          
-    }
+    
 
 } # end of the class
