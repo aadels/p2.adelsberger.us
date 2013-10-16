4c51c055d10e0b7fb8368d0151782c20550c24a2
ann adelsberger <adelsbergerann@gmail.com>
2013-10-16 18:55:00 -0400

    added header in view and added to controller but commented out

--- controllers/c_practice.php
+++ controllers/c_practice.php
@@ -1,12 +1,12 @@
 <?php
 class practice_controller extends base_controller{
-
+public function test_db(){
 # Our SQL command
 $q = "INSERT INTO users SET 
     first_name = 'Sam', 
     last_name = 'Seaborn',
     email = 'seaborn@whitehouse.gov'";
-
+}
 # Run the command
 echo DB::instance(DB_NAME)->query($q);
 
