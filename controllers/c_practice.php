d31f075c7e12c1f658bcbca030d1923c93d1b726
ann adelsberger <adelsbergerann@gmail.com>
2013-10-16 18:46:21 -0400

    added user to DB

--- controllers/c_practice.php
+++ controllers/c_practice.php
@@ -1,15 +1,11 @@
 <?php
 class practice_controller extends base_controller{
-	# Our SQL command
 
-public function test_db(){
-
+# Our SQL command
 $q = "INSERT INTO users SET 
     first_name = 'Sam', 
     last_name = 'Seaborn',
     email = 'seaborn@whitehouse.gov'";
-
- echo $q;
 
 # Run the command
 echo DB::instance(DB_NAME)->query($q);
