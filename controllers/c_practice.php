0f8e4f3beae0fbc08fceecf34fd62421708380a0
ann adelsberger <adelsbergerann@gmail.com>
2013-10-16 18:42:24 -0400

    added function line back

--- controllers/c_practice.php
+++ controllers/c_practice.php
@@ -2,10 +2,16 @@
 class practice_controller extends base_controller{
 	# Our SQL command
 
+public function test_db(){
+
 $q = "INSERT INTO users SET 
     first_name = 'Sam', 
     last_name = 'Seaborn',
     email = 'seaborn@whitehouse.gov'";
 
+ echo $q;
+
 # Run the command
 echo DB::instance(DB_NAME)->query($q);
+
+}
