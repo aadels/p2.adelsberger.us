de6813eff08b35ca6e6c1884f9c1d020881e8ed5
ann adelsberger <adelsbergerann@gmail.com>
2013-10-16 18:30:53 -0400

    parctice adding to database

--- controllers/c_practice.php
+++ controllers/c_practice.php
@@ -2,16 +2,10 @@
 class practice_controller extends base_controller{
 	# Our SQL command
 
-public function test_db(){
-
 $q = "INSERT INTO users SET 
     first_name = 'Sam', 
     last_name = 'Seaborn',
     email = 'seaborn@whitehouse.gov'";
 
-    echo $q;
- 
-
 # Run the command
-DB::instance(DB_NAME)->query($q);
-}
+echo DB::instance(DB_NAME)->query($q);
