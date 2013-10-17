748d8b5efdd098b0847898db99af74c2daf14e05
ann adelsberger <adelsbergerann@gmail.com>
2013-10-17 07:19:18 -0400

    syntax error

--- controllers/c_practice.php
+++ controllers/c_practice.php
@@ -6,7 +6,7 @@ $q = "INSERT INTO users SET
     first_name = 'Sam', 
     last_name = 'Seaborn',
     email = 'seaborn@whitehouse.gov'";
-}
+
 # Run the command
 echo DB::instance(DB_NAME)->query($q);
 
