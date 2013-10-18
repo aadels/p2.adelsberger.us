27c80057c86e4b3525785f8f7ca9e0de60b6fe66
ann adelsberger <adelsbergerann@gmail.com>
2013-10-18 17:25:06 -0400

    added error message to view

--- views/v_users_login.php
+++ views/v_users_login.php
@@ -7,8 +7,12 @@
 
 	Password<br>
 	<input type='password' name = 'password'>
-
 	<br><br>
+
+	<?php if(isset($error)): ?>
+		<div class='error'>
+			Login failed. Please double check your email and password.
+		</div>
 
 	<input type='submit' value='Log in'>
 
