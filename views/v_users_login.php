e55d6240f0a89aa031d55d159ee52df0644d8b3c
ann adelsberger <adelsbergerann@gmail.com>
2013-10-18 21:46:12 -0400

    removed error message

--- views/v_users_login.php
+++ views/v_users_login.php
@@ -11,12 +11,7 @@
 	<input type='password' name = 'password'>
 	<br><br>
 
-	<?php if(isset($error)): ?>
-		<div class='error'>
-			Login failed. Please double check your email and password.
-		</div>
-
-	<input type='submit' value='Log in'>
+	
 
 
 </form>
