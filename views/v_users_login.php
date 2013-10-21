1347139cd1673947d4fd48e805c6bde3c420ae54
ann adelsberger <adelsbergerann@gmail.com>
2013-10-21 14:11:02 -0400

    added error statemnet to login view

--- views/v_users_login.php
+++ views/v_users_login.php
@@ -10,6 +10,13 @@
 	<input type='password' name = 'password'>
 	<br><br>
 
+	<?php if(isset($error)): ?>
+		<div class='error'>
+			Login Failed. Please double check your email and password.
+		</div>
+		<br>
+	<?php endif; ?>
+
 	<input type='submit' value='Log in'>
 
 
