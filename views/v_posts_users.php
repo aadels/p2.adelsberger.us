94af0f6e589d9fac22e8b7b249be27cc4fcee780
ann adelsberger <adelsbergerann@gmail.com>
2013-10-23 19:39:45 -0400

    Syntax error

--- views/v_posts_users.php
+++ views/v_posts_users.php
@@ -4,14 +4,13 @@
 	<?=$user['first_name']?> <?=$user['last_name']?>
 
 	<!-- If there is a connection with this user, sho an unfollow link -->
-	<? if(isset($connections[$user['user_id']]): ?>
-
-		<a href='/posts/unfollow/<?=$user['user_id']?>'>Unfollow</a>
+	<? if(isset($connections[$user['user_id']])): ?>
+        <a href='/posts/unfollow/<?=$user['user_id']?>'>Unfollow</a>
 
 	<!-- Otherwise, show the folllow link -->
 	<? else: ?>
-		<a href='/posts/follow/<?$users['user_id']?>'>Follow</a>
-	<? endif; ?>
+        <a href='/posts/follow/<?=$user['user_id']?>'>Follow</a>
+    <? endif; ?>
 
 	<br><br>
 
