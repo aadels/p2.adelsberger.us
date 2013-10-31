aa386610dd856c20631ee5eaa1bbaffac6f2341e
ann adelsberger <adelsbergerann@gmail.com>
2013-10-31 14:40:07 -0400

    added required to signup

--- views/v_users_signup.php
+++ views/v_users_signup.php
@@ -1,21 +1,21 @@
 <h2>Sign Up</h2>
 
-<form method='POST' action='/users/p_signup'>
+<form class=formfields method='POST' action='/users/p_signup'>
 
     First Name<br>
-    <input type='text' name='first_name'>
+    <input type='text' name='first_name' required>
     <br><br>
 
     Last Name<br>
-    <input type='text' name='last_name'>
+    <input type='text' name='last_name' required>
     <br><br>
 
     Email<br>
-    <input type='text' name='email'>
+    <input type='text' name='email' required>
     <br><br>
 
     Password<br>
-    <input type='password' name='password'>
+    <input type='password' name='password' required>
     <br><br>
 
     <input type='submit' value='Sign up'>
