0a7db1ccd38f6b9ccce15fbe1242cb1b862605ee
ann adelsberger <adelsbergerann@gmail.com>
2013-11-02 13:16:01 -0400

    edited profile view

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -7,8 +7,8 @@
         <form role="form" method='POST' enctype="multipart/form-data" action='/users/profile_image/'>
                 <img src="/uploads/avatars/<?= $user->image ?>" alt="<?=$user->first_name . ' ' . $user->last_name ?>">                    
                     <div class="form-group">
-                            <label for="exampleInputFile">Your Profile Image</label>
-                            <input type="file" id="avatar" name="avatar">
+                            <label for="exampleInputFile">Your Profile Image</label><br />
+                            <input type="file" id="image" name="image">
                     </div>
                     <button type="submit" class="btn btn-custom">Update Your Profile Image</button>
         </form>   
