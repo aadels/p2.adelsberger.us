3cf7109624f5c34fa95404c9184d269998828485
ann adelsberger <adelsbergerann@gmail.com>
2013-11-05 00:45:36 -0500

    styled posts index

--- views/v_posts_index.php
+++ views/v_posts_index.php
@@ -4,19 +4,23 @@
 </div>
 
 
-<h1>Hi, <?=$user->first_name?>. Here are the posts from users you are following.</h1>
+<h2>Hi, <?=$user->first_name?>. Here are the posts from users you are following.</h2>
 
 <?php foreach($posts as $post): ?>
 
 <article>
+	<div class="row">
+        <div class="col-xs-2 col-lg-1">
+        	<img src="/uploads/avatars/<?=$post['image']?>">
+        </div>
 
-	<h2> <?=$post['first_name']?> <?=$post['last_name'] ?> posted: </h2>
+			<h3> <?=$post['first_name']?> <?=$post['last_name'] ?> posted: </h3>
 
-	<p><?=$post['content'] ?></p>
+			<p><?=$post['content'] ?></p>
 
-	<time datetime="<?=Time::display($post['created'],'Y-m-d G:i')?>">
-		<?=Time::display($post['created'])?>
-	</time>
+			<time datetime="<?=Time::display($post['created'],'Y-m-d G:i')?>">
+				<x-small><?=Time::display($post['created'])?></x-small>
+			</time>
 
 </article>
 
