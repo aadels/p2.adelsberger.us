09a0f248ac1b0af3a17e0f5de9296d70b3419a0d
ann adelsberger <adelsbergerann@gmail.com>
2013-11-05 16:46:47 -0500

    edited signup style

--- views/v_users_signup.php
+++ views/v_users_signup.php
@@ -4,19 +4,20 @@
 
     <div class="form-group">
             <label for="first_name">First Name</label>
-            <input type="text" class="form-control" id="first_name" name="first_name" placeholder="First Name" <?php if(isset($_POST['first_name'])) echo "value = '". $_POST['first_name'] ."'"?>>
+            <input type="text" class="form-control" id="first_name" name="first_name"<?php if(isset($_POST['first_name'])) echo "value = '". $_POST['first_name'] ."'"?>>
     </div>
+    
     <div class="form-group">
         <label for="last_name">Last Name</label>
-        <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Last Name" <?php if(isset($_POST['last_name'])) echo "value = '". $_POST['last_name'] ."'"?>>
+        <input type="text" class="form-control" id="last_name" name="last_name"<?php if(isset($_POST['last_name'])) echo "value = '". $_POST['last_name'] ."'"?>>
     </div>
     <div class="form-group">
         <label for="email">Email</label>
-        <input type="email" class="form-control" id="email" name="email" placeholder="Email" <?php if(isset($_POST['email'])) echo "value = '". $_POST['email'] ."'"?>>
+        <input type="email" class="form-control" id="email" name="email"<?php if(isset($_POST['email'])) echo "value = '". $_POST['email'] ."'"?>>
     </div>
     <div class="form-group">
         <label for="first_name">Password</label>
-        <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+        <input type="password" class="form-control" id="password" name="password">
     </div> 
 
 
