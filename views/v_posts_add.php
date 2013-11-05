16649ebbfb5b660defba675a050353c553c1b0c5
ann adelsberger <adelsbergerann@gmail.com>
2013-11-05 15:36:19 -0500

    bootstrap form style to add post

--- views/v_posts_add.php
+++ views/v_posts_add.php
@@ -1,18 +1,18 @@
 <form method='POST' action='/posts/p_add'>
+    <div class="form-group">
+        <label for='content'>New Post:</label>
+        <br>
+        <textarea "form-control" rows="3" ='content' id='content'></textarea>
 
-    <label for='content'>New Post:</label>
-    <br>
-    <textarea name='content' id='content'></textarea>
-
-     <?php if(isset($error)): ?>
-        <div class='error'>
-            Post failed. Please add a post!
+         <?php if(isset($error)): ?>
+            <div class='error'>
+                Post failed. Please add a post!
+            </div>
+            
+        <?php endif; ?>
         </div>
-        
-    <?php endif; ?>
-
-    <br><br>
-    <input type='submit' value='New post'>
-
+        <br><br>
+        <input type='submit' value='New post'>
 </form> 
+
 
