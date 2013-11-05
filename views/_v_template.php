fbca5eeb7b21b4b478758ac659075401e5013e92
ann adelsberger <adelsbergerann@gmail.com>
2013-11-04 20:25:19 -0500

    added styles to menu and main body

--- views/_v_template.php
+++ views/_v_template.php
@@ -5,8 +5,9 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	    <script src="js/respond.js"></script>	
+	    <link href="/css/styles.css" rel="stylesheet">
 	    
-	    <!-- Bootstrapp -->
+	    <!-- Bootstrap -->
 	    <link href="/css/bootstrap.min.css" rel="stylesheet">
 
 	    <!-- fonts -->
@@ -18,59 +19,62 @@
 	</head>
 
 	<body>	
-		<div class="container">
+		
 		<!-- row 1: navigation -->
-		<div id='menu'>
-			<a href='/'>Home</a>
+			<div id='menu'>
 
-			<!-- Menu for users who are logged in-->
-			<div class="row">
-				<nav class="collapse navbar-collapse pull-right" role="navigation">
-					<div class="navbar-header">
-	                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#collapse">
-	                        <span class="sr-only">Toggle navigation</span>
-	                        <span class="icon-bar"></span>
-	                        <span class="icon-bar"></span>
-	                        <span class="icon-bar"></span>
-	                    </button>
-	            	</div>
-	            	<div class="collapse navbar-collapse" id="collapse">
-						<ul class="nav navbar-nav">
-							<?php if($user): ?>
-						        <li>
-						        	<a href='/posts'>View Feed</a> 
-						        </li>
-						        <li> 
-						        	<a href='/posts/add'>Post</a> 
-						        </li>
-						        <li>                       
-						        	<a href='/posts/users'>Follow Others</a> 
-						        </li>
-						        <li>                    
-									<a href='/users/logout'>Logout</a>
-								</li>
-								<li>
-									<a href='/users/profile'>Profile</a>
-								</li>
+				<!-- Menu for users who are logged in-->
+				<div class="row">
+					<nav class="collapse navbar-collapse pull-right" role="navigation">
+						<div class="navbar-header">
+		                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#collapse">
+		                        <span class="sr-only">Toggle navigation</span>
+		                        <span class="icon-bar"></span>
+		                        <span class="icon-bar"></span>
+		                        <span class="icon-bar"></span>
+		                    </button>
+		                    <img src="../img/Yak2.png" a href="/" class="navbar-logo"> YaketyYak/a>
+		            	</div>
+		            	<div class="collapse navbar-collapse" id="collapse">
+							<ul class="nav navbar-nav">
+								<?php if($user): ?>
+							        <li>
+							        	<a href='/posts'>View Feed</a> 
+							        </li>
+							        <li> 
+							        	<a href='/posts/add'>Post</a> 
+							        </li>
+							        <li>                       
+							        	<a href='/posts/users'>Follow Others</a> 
+							        </li>
+							        <li>                    
+										<a href='/users/logout'>Logout</a>
+									</li>
+									<li>
+										<a href='/users/profile'>Profile</a>
+									</li>
 
-							<!-- Menu for users who are not logged in-->
-							<?php else: ?>
-								<li>
-									<a href='/users/signup'>Sign up</a>
-								</li>
-								<li>
-									<a href='/users/login'>Log in</a>
-								</li>
-							<?php endif; ?>
-						</ul>
-					</div>
-				</nav>
-			</div>	
-		</div><!--close container-->
+								<!-- Menu for users who are not logged in-->
+								<?php else: ?>
+									<li>
+										<a href='/users/signup'>Sign up</a>
+									</li>
+									<li>
+										<a href='/users/login'>Log in</a>
+									</li>
+								<?php endif; ?>
+							</ul>
+						</div>
+					</nav>
+				</div><!--close row-->	
+			</div><!--close menu-->	
+			
 
 		<div class="container">
-			<?php if(isset($content)) echo $content; ?>
-		</div>
+			<div class="row well well-lg">
+				<?php if(isset($content)) echo $content; ?>
+			</div><!-- well -->
+		</div><!-- container-->
 
 	    <div id="footer">
             <div class="container">
