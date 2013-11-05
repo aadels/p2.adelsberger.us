688cbf9ca659d13779891e37b13501b70d045a8f
ann adelsberger <adelsbergerann@gmail.com>
2013-11-05 00:28:43 -0500

    fixed navbar collapse

--- views/_v_template.php
+++ views/_v_template.php
@@ -27,7 +27,7 @@
 					<nav class="navbar" role="banner">
 						<div class="container">
 							<div class="navbar-header">
-			                    <button class="navbar-toggle" data-toggle="collapse" data-target="#collapse">
+			                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
 			                        <span class="sr-only">Toggle navigation</span>
 			                        <span class="icon-bar"></span>
 			                        <span class="icon-bar"></span>
