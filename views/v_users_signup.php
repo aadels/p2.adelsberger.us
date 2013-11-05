2e50c9ff4c6f76e4ab26393457a32e26b756a62e
ann adelsberger <adelsbergerann@gmail.com>
2013-11-05 16:23:27 -0500

    button styling

--- views/v_users_signup.php
+++ views/v_users_signup.php
@@ -35,6 +35,6 @@
 
     <?php endif; ?>
 
-    <button type="button" class="btn-custom btn-block">Sign up</button>
+    <button type="button" class="btn btn-custom btn-lg btn-block">Sign up</button>
 
 </form>
