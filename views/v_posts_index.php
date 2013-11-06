32b53a45de001109e0eb5653c2c4d64ef2755a77
ann adelsberger <adelsbergerann@gmail.com>
2013-11-05 23:54:15 -0500

    edited grid/offset

--- views/v_posts_index.php
+++ views/v_posts_index.php
@@ -1,10 +1,10 @@
 
 <div class="row">
-		<aside class="col-xs-2 col-lg-1">
+		<aside class="col-s-1 col-lg-1">
         	<img class="img-rounded img-post" src="/uploads/avatars/<?= $user->image ?>">
         </aside>
-        <article class="col-xs-10 col-lg-11 ">
-        	<h2>Hi, <?=$user->first_name?>. Here are the posts from users you are following.</h2>
+        <article class="col-lg-offset-1 col-lg-10 col-lg-offset-1 col-lg-11 ">
+        	<h3>Hi, <?=$user->first_name?>. Here are the posts from users you are following.</h3>
         </article>
 </div>
 
@@ -21,7 +21,7 @@
         <aside class="col-xs-2 col-lg-1">
         	<img class="img-rounded img-post" src="/uploads/avatars/<?=$post['image']?>">
         </aside>
-    	<article class="col-xs-9 col-sm-offset-1 col-lg-10 col-lg-offset-1 ">
+    	<article class="col-xs-9 col-xs-offset-1 col-lg-10 col-lg-offset-1 ">
         	<div class="well post">
 				<h4> <?=$post['first_name']?> <?=$post['last_name'] ?> posted: </h4>
 				<p><?=$post['content'] ?></p>
