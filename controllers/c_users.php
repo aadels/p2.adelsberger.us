1c6c233a3db6111ab8c917ee3632bdfc3d685395
ann adelsberger <adelsbergerann@gmail.com>
2013-11-05 22:14:20 -0500

    invaild error message

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -228,3 +228,30 @@ class users_controller extends base_controller {
 
 
 } # end of the class
+class customException extends Exception{
+      public function errorMessage()
+        {
+        //error message
+        $errorMsg = 'Error on line '.$this->getLine().' in '.$this->getFile()
+        .': <b>'.$this->getMessage().'</b> is not a valid E-Mail address';
+        return $errorMsg;
+        }
+}
+
+$email = "someone@example...com";
+
+    
+    {
+      //check if
+      if(filter_var($email, FILTER_VALIDATE_EMAIL) === FALSE)
+    {
+        //throw exception if email is not valid
+        throw new customException($email);
+        }
+      }
+
+catch (customException $e)
+      {
+        //display custom message
+        echo $e->errorMessage();
+      }//end of the class
