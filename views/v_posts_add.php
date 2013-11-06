e7ca4bcd7054e145e237cebb8d00e86eb66c88b8
ann adelsberger <adelsbergerann@gmail.com>
2013-11-05 21:27:58 -0500

    moved div eend tag to save error handling function

--- views/v_posts_add.php
+++ views/v_posts_add.php
@@ -3,14 +3,15 @@
         <label for='content'>New Post:</label>
         <br>
         <textarea "form-control" rows="3" ='content' id='content'></textarea>
-
-         <?php if(isset($error)): ?>
+        </div>
+        
+        <?php if(isset($error)): ?>
             <div class='error'>
                 Post failed. Please add a post!
             </div>
             
         <?php endif; ?>
-        </div>
+        
         <br><br>
         <input type='submit' value='New post'>
 </form> 
