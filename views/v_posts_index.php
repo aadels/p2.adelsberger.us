b4aaa0b575d05272f7cbf3eba3b3d318c90c10a6
ann adelsberger <adelsbergerann@gmail.com>
2013-11-05 23:11:17 -0500

    changed grid

--- views/v_posts_index.php
+++ views/v_posts_index.php
@@ -14,12 +14,12 @@
 
 <article>
 	<div class="row">
-        <div class="col-xs-3 col-lg-2">
+        <div class="col-xs-2 col-lg-1">
         	<img class="img-rounded img-post" src="/uploads/avatars/<?=$post['image']?>">
         </div>
-        	<div class="col-xs-9 col-lg-10">
+        	<div class="col-xs-10 col-lg-11">
 	        	<div class="well post">
-					<h3> <?=$post['first_name']?> <?=$post['last_name'] ?> posted: </h3>
+					<h4> <?=$post['first_name']?> <?=$post['last_name'] ?> posted: </h4>
 					<p><?=$post['content'] ?></p>
 					<time datetime="<?=Time::display($post['created'],'Y-m-d G:i')?>">
 						<small><?=Time::display($post['created'])?></small>
