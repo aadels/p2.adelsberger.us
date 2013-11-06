01c429fded65e5e4afba0b402005a6806e6b76ea
ann adelsberger <adelsbergerann@gmail.com>
2013-11-05 21:17:52 -0500

    Revert 039f610..7176cc4
    This rolls back to commit 039f6105775fd6c118ddaa7a03fe90add89fb208.

--- views/v_users_signup.php
+++ views/v_users_signup.php
@@ -2,24 +2,22 @@
 
 <form role="form" method='POST' action='/users/p_signup'>
 
-     <form role="form" method="POST" action="/users/signup">
-        <div class="form-group">
+    <div class="form-group">
             <label for="first_name">First Name</label>
-            <input type="text" class="form-control" id="first_name" name="first_name" placeholder="Enter first name" <?php if(isset($_POST['first_name'])) echo "value = '". $_POST['first_name'] ."'"?>>
-        </div>
-        <div class="form-group">
-            <label for="last_name">Last Name</label>
-            <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Enter last name" <?php if(isset($_POST['last_name'])) echo "value = '". $_POST['last_name'] ."'"?>>
-        </div>
-        <div class="form-group">
-            <label for="email">Email</label>
-            <input type="email" class="form-control" id="email" name="email" placeholder="Enter email" <?php if(isset($_POST['email'])) echo "value = '". $_POST['email'] ."'"?>>
-        </div>
-        <div class="form-group">
-            <label for="first_name">Password</label>
-            <input type="password" class="form-control" id="password" name="password" placeholder="Enter password">
-        </div>  
-         
+            <input type="text" class="form-control" id="first_name" name="first_name" placeholder="First Name" <?php if(isset($_POST['first_name'])) echo "value = '". $_POST['first_name'] ."'"?>>
+    </div>
+    <div class="form-group">
+        <label for="last_name">Last Name</label>
+        <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Last Name" <?php if(isset($_POST['last_name'])) echo "value = '". $_POST['last_name'] ."'"?>>
+    </div>
+    <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" class="form-control" id="email" name="email" placeholder="Email" <?php if(isset($_POST['email'])) echo "value = '". $_POST['email'] ."'"?>>
+    </div>
+    <div class="form-group">
+        <label for="first_name">Password</label>
+        <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+    </div> 
 
 
     <?php if(isset($error) && $error == 'blank-fields'): ?>
@@ -37,6 +35,6 @@
 
     <?php endif; ?>
 
-    <button type="button" class="btn btn-custom btn-lg btn-block">Sign up</button>
+    <button type="button" class="btn btn-default btn-lg btn-block">Sign up</button>
 
 </form>
