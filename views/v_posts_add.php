47200598966bb80f20640362f907f3f450eff6db
ann adelsberger <adelsbergerann@gmail.com>
2013-11-21 18:45:43 -0500

    Update P2  to use js add js docs

--- views/v_posts_add.php
+++ views/v_posts_add.php
@@ -1,18 +1,8 @@
-<form method='POST' action='/posts/p_add'>
+<form method='post' action='/posts/p_add'>
+    <textarea name='content'></textarea><br><br>
+    <input type='Submit' value='Add new post'>
+</form>
 
-    <label for='content'>New Post:</label>
-    <br>
-    <textarea name='content' id='content'></textarea>
-
-     <?php if(isset($error)): ?>
-        <div class='error'>
-            Post failed. Please add a post!
-        </div>
-        
-    <?php endif; ?>
-
-    <br>
-    <input type='submit' value='New post'>
-
-</form> 
+<!-- Ajax results will go here -->
+<div id='results'></div>
 
