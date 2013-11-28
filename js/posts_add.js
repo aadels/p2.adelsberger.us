b720afe79664bf24aa77f1def1a5149b0a7b53b3
ann adelsberger <adelsbergerann@gmail.com>
2013-11-28 07:03:43 -0500

    add madlib css

--- js/posts_add.js
+++ js/posts_add.js
@@ -3,10 +3,7 @@
                     
     type: 'post',
     url: 'posts/p_add/',
-    beforeSend: function() {   
-    //Display a loading message while waiting for the ajax call to complete
-        $('#results').html("Loading...");
-    },
+
     //Hide form and display results
     success: function(response) {
         $(".madlib_form").hide();
