7830e63ce13ff29126fb63f3e86e4221cae1ff1e
ann adelsberger <adelsbergerann@gmail.com>
2013-11-29 16:59:37 -0500

    testing form in view

--- views/v_posts_users.php
+++ views/v_posts_users.php
@@ -1,22 +1,57 @@
 <h1>Follow Users</h1>
 
-<? foreach ($users as $user): ?>
+        <form class="madlib_form" method='post'>
+        	<fieldset id= "form-fieldset">
+	            Adjective: <input type="text" id='madlib' name="input1" required size=20><br>
+	            Plural Noun: <input type="text" id='madlib' name="input2" required size=20><br>
+	            Femenine first name: <input type="text" id='madlib' name="input3" required size=20><br>
+	            Last name: <input type="text" id='madlib' name="input4" required size=20><br>
+	            Noun: <input type="text" id='madlib' name="input5" required size=20><br>
+	            A place: <input type="text" id='madlib' name="input6" required size=20><br>
+	            A special event: <input type="text"  id='madlib' name="input7" required size=20><br>
+	            Verb: <input type="text" id='madlib' name="input8" required size=20><br>
+	            Noun: <input type="text" id='madlib' name="input9" required size=20><br>
+	            Verb in the present tense: <input type="text" id='madlib' name="input10" required size=20><br>
+	            Noun: <input type="text" id='madlib' name="input11" required size=20><br>
+	            Noun: <input type="text" id='madlib' name="input12" required size=20><br>
+	            Noun: <input type="text" id='madlib' name="input13" requiredsize=20><br>
+	            Noun: <input type="text"  id='madlib' name="input14" required size=20><br>
+	            Verb: <input type="text" id='madlib' name="input15" required size=20><br>
 
-	<!-- Print user's name -->
-	<h2><?=$user['first_name']?> <?=$user['last_name']?></h2>
-	
+	            <input type='submit' class="button" value='See Mad-lib!'> 
+	        </fieldset>          
+        </form>
 
-	<!-- If there is a connection with this user, sho an unfollow link -->
-	<? if(isset($connections[$user['user_id']])): ?>
-        <a href='/posts/unfollow/<?=$user['user_id']?>'>Unfollow</a>
+        <fieldset id= "result-fieldset">
+            <div id='results'></div>
+        </fieldset>
+        
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script type="text/javascript" src='js/jquery.form.js'></script>
+        <script type="text/javascript" src='js/jquery.validate.js'></script>
+        
+        <script>
+        
+                var options = {
+                    
+                    type: 'post',
+                    url: 'process2.php',
+                    beforeSend: function() {   
+                        // Display a loading message while waiting for the ajax call to complete
+                        $('#results').html("Loading...");
+                    },
+                    
+                    success: function(response) {
+                        $(".madlib_form").hide();
+                        $('#results').html(response);
+                	}
+                };
 
-	<!-- Otherwise, show the folllow link -->
-	<? else: ?>
-        <a href='/posts/follow/<?=$user['user_id']?>'>Follow</a>
-    <? endif; ?>
-
-	<br><br>
-
-<? endforeach; ?>
+                $('form').ajaxForm(options);
+               
+     
+        </script>
+        
+        
 
 
