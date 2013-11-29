2fdc3e237b5d74fc2c026e9a6e42c65d9fbc4c55
ann adelsberger <adelsbergerann@gmail.com>
2013-11-29 12:06:42 -0500

    changed p_add

--- controllers/c_posts.php
+++ controllers/c_posts.php
@@ -32,18 +32,20 @@ class posts_controller extends base_controller{
 	public function p_add() {
 
 	    // Set up the data
-	    $_POST['user_id']  = $this->user->user_id;
-	    $_POST['created']  = Time::now();
-	    $_POST['modified'] = Time::now();
+	  echo "O ". ($_POST['input1'])." heart, hid with a(n) ". ($_POST['input2'])." face! <br>
+			Did ever dragon keep so fair a ".($_POST['input3']).".
+			 Beautiful ".($_POST['input4'])."! Fiend angelical! <br>
+			Dove-feathered"." ".($_POST['input5'])."! " . ucfirst($_POST['input6'])."ish-ravening lamb! <br>
+			Despised ".($_POST['input7'])." of divinest show! <br>
+			Just opposite to what thou justly seem’st.<br> 
+			A ".($_POST['input8'])." saint, a(n) ". ($_POST['input9'])." villain! <br>
+			O nature, what hadst thou to do in ". ($_POST['input10']). "<br>
+			 When thou didst bower the spirit of a ". ($_POST['input11']). "<br> 
+			In mortal paradise of such ". ($_POST['input12'])." flesh? <br> 
+			Was ever book containing such ". ($_POST['input13']) . "matter <br> 
+			So fairly bound? O, that deceit should dwell <br> 
+			In such a ". ($_POST['input14'])." palace!";
 
-	    // Insert the post
-	    DB::instance(DB_NAME)->insert('posts',$_POST);
-
-	    // Send a simple message back
-	    //echo "New post was added on ".Time::display(Time::now());
-	    $view = new View('v_posts_p_add');
-
-	    $view->created = Time::display(Time::now());
 	    echo $view;
 	}
 
