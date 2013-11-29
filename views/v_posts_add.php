24d2e29ebc59b49ec4a2e5098bb081fa1fc6e371
ann adelsberger <adelsbergerann@gmail.com>
2013-11-29 13:33:28 -0500

    change add post

--- views/v_posts_add.php
+++ views/v_posts_add.php
@@ -1,18 +1,23 @@
 <form method='POST' action='/posts/p_add'>
 
-    <label for='content'>New Post:</label>
-    <br>
-    <textarea name='content' id='content'></textarea>
+    <fieldset id= "form-fieldset">
+        Animal: <input type="text" id='madlib' name="input1" required size=20><br>
+        Adjective: <input type="text" id='madlib' name="input2" required size=20><br>
+        Room in a house: <input type="text" id='madlib' name="input3" required size=20><br>
+        Occupation: <input type="text" id='madlib' name="input4" required size=20><br>
+        Type of bird: <input type="text" id='madlib' name="input5" required><br>
+        Type of dog: <input type="text" id='madlib' name="input6" required><br>
+        Noun: <input type="text"  id='madlib' name="input7" required><br>
+        Adjective: <input type="text" id='madlib' name="input8" required><br>
+        Adjective: <input type="text" id='madlib' name="input9" required><br>
+        Name of a city: <input type="text" id='madlib' name="input10" required><br>
+        Animal: <input type="text" id='madlib' name="input11" required><br>
+        Adjective: <input type="text" id='madlib' name="input12" required><br>
+        Adjective: <input type="text" id='madlib' name="input13" required><br>
+        Adjective: <input type="text" id='madlib' name="input14" required><br>
 
-     <?php if(isset($error)): ?>
-        <div class='error'>
-            Post failed. Please add a post!
-        </div>
-        
-    <?php endif; ?>
-
-    <br>
-    <input type='submit' value='New post'>
-
+        <input type='submit' class="button" value='See Mad-lib!'> 
 </form> 
 
+<!-- Ajax results will go here -->
+<div id='results'></div>
