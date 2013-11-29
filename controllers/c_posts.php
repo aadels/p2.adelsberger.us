38e539b2d80b85b4fc885853b9e3384c6f4808b7
ann adelsberger <adelsbergerann@gmail.com>
2013-11-29 12:45:55 -0500

    Revert 7203b30..83b72a0
    This rolls back to commit 7203b301ea1d36dee1114f5fdb13088f14a8b88a.

--- controllers/c_posts.php
+++ controllers/c_posts.php
@@ -14,14 +14,12 @@ class posts_controller extends base_controller{
 
 	    # Setup view
 	    $this->template->content = View::instance('v_posts_add');
-	    $this->template->title   = "Mad-lib";
+	    $this->template->title   = "Add a new post";
 
 	    # Load JS files
 	    $client_files_body = Array(
 	        "/js/jquery.form.js",
 	        "/js/posts_add.js"
-	        "/js/respond.js"
-	        "/js/jquery-1.10.2.min.js"
 	    );
 
 	    $this->template->client_files_body = Utils::load_client_files($client_files_body);   
@@ -33,20 +31,18 @@ class posts_controller extends base_controller{
 	public function p_add() {
 
 	    // Set up the data
-	  "O ". ($_POST['input1'])." heart, hid with a(n) ". ($_POST['input2'])." face! <br>
-			Did ever dragon keep so fair a ".($_POST['input3']).".
-			 Beautiful ".($_POST['input4'])."! Fiend angelical! <br>
-			Dove-feathered"." ".($_POST['input5'])."! " . ucfirst($_POST['input6'])."ish-ravening lamb! <br>
-			Despised ".($_POST['input7'])." of divinest show! <br>
-			Just opposite to what thou justly seem’st.<br> 
-			A ".($_POST['input8'])." saint, a(n) ". ($_POST['input9'])." villain! <br>
-			O nature, what hadst thou to do in ". ($_POST['input10']). "<br>
-			 When thou didst bower the spirit of a ". ($_POST['input11']). "<br> 
-			In mortal paradise of such ". ($_POST['input12'])." flesh? <br> 
-			Was ever book containing such ". ($_POST['input13']) . "matter <br> 
-			So fairly bound? O, that deceit should dwell <br> 
-			In such a ". ($_POST['input14'])." palace!";
+	    $_POST['user_id']  = $this->user->user_id;
+	    $_POST['created']  = Time::now();
+	    $_POST['modified'] = Time::now();
 
+	    // Insert the post
+	    DB::instance(DB_NAME)->insert('posts',$_POST);
+
+	    // Send a simple message back
+	    //echo "New post was added on ".Time::display(Time::now());
+	    $view = new View('v_posts_p_add');
+
+	    $view->created = Time::display(Time::now());
 	    echo $view;
 	}
 
