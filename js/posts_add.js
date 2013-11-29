f62bcdf58b9dfd545c639c59453f486e4e1dd647
ann adelsberger <adelsbergerann@gmail.com>
2013-11-29 10:45:50 -0500

    testing p3 in p2 framepwrk

--- js/posts_add.js
+++ js/posts_add.js
@@ -3,7 +3,10 @@
                     
     type: 'post',
     url: 'posts/p_add/',
-
+    beforeSend: function() {   
+    //Display a loading message while waiting for the ajax call to complete
+        $('#results').html("Loading...");
+    },
     //Hide form and display results
     success: function(response) {
         $(".madlib_form").hide();
