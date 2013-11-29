a56df976255a4e57fa997855e147d659108ad9f0
ann adelsberger <adelsbergerann@gmail.com>
2013-11-29 11:34:06 -0500

    Revert 7203b30..38c0eb5
    This rolls back to commit 7203b301ea1d36dee1114f5fdb13088f14a8b88a.

--- controllers/c_posts.php
+++ controllers/c_posts.php
@@ -19,8 +19,7 @@ class posts_controller extends base_controller{
 	    # Load JS files
 	    $client_files_body = Array(
 	        "/js/jquery.form.js",
-	        "/js/posts_add.js",
-	        "/js/respond.js"	
+	        "/js/posts_add.js"
 	    );
 
 	    $this->template->client_files_body = Utils::load_client_files($client_files_body);   
@@ -32,26 +31,77 @@ class posts_controller extends base_controller{
 	public function p_add() {
 
 	    // Set up the data
-	    echo "O ". ($_POST['input1'])." heart, hid with a(n) ". ($_POST['input2'])." face! <br>
-		Did ever dragon keep so fair a ".($_POST['input3']).".
-		 Beautiful ".($_POST['input4'])."! Fiend angelical! <br>
-		Dove-feathered"." ".($_POST['input5'])."! " . ucfirst($_POST['input6'])."ish-ravening lamb! <br>
-		Despised ".($_POST['input7'])." of divinest show! <br>
-		Just opposite to what thou justly seem’st.<br> 
-		A ".($_POST['input8'])." saint, a(n) ". ($_POST['input9'])." villain! <br>
-		O nature, what hadst thou to do in ". ($_POST['input10']). "<br>
-		 When thou didst bower the spirit of a ". ($_POST['input11']). "<br> 
-		In mortal paradise of such ". ($_POST['input12'])." flesh? <br> 
-		Was ever book containing such ". ($_POST['input13']) . "matter <br> 
-		So fairly bound? O, that deceit should dwell <br> 
-		In such a ". ($_POST['input14'])." palace!";
-		// Insert the post
-    	
-		}
+	    $_POST['user_id']  = $this->user->user_id;
+	    $_POST['created']  = Time::now();
+	    $_POST['modified'] = Time::now();
 
-	
+	    // Insert the post
+	    DB::instance(DB_NAME)->insert('posts',$_POST);
+
+	    // Send a simple message back
+	    //echo "New post was added on ".Time::display(Time::now());
+	    $view = new View('v_posts_p_add');
+
+	    $view->created = Time::display(Time::now());
+	    echo $view;
+	}
+
+	public function users() {
+
+		//Set up the view
+		$this->template->content = View::instance("v_posts_users");
+		$this->template->title   = "Users";
+
+		#Build a query to get all of the users
+		$q = "SELECT * FROM users";
+
+		//Execute the query to get all of teh users.
+		//Store the resulting arraty in the variable $users
+		$users = DB::instance(DB_NAME)->select_rows($q);
+
+		//Build the query to figure outwhat connections the user already has
+		$q = "SELECT * FROM users_users WHERE user_id = ".$this->user->user_id;
+
+		//Execute this query with the select_array method.
+		//The select_array will return ourresults in an array and use the "users_id Followed" field as the index.
+		//This will come in handy when we get to the view. 
+		//Store the results (an array) in teh variable $connections
+
+		$connections = DB::instance(DB_NAME)->select_array($q, 'user_id_followed');
+
+		//Pass data (users and connections) to the view
+		$this->template->content->users 		= $users;
+		$this->template->content->connections 	= $connections;
+
+		//Render the view
+		echo $this->template;
+	}
 		
+	public function follow($user_id_followed) {
+		//Prepare the dtat array to be inserted
+		$data = Array(
+			"created" => Time::now(),
+			"user_id" => $this->user->user_id,
+			"user_id_followed" => $user_id_followed
+			);
 
+		//Do the insert
+		DB::instance(DB_NAME)->insert('users_users', $data);
+
+		//Sent them back
+		Router::redirect("/posts/users");
+	}
+
+	public function unfollow($user_id_followed) {
+
+    	//Delete this connection
+    	$where_condition = 'WHERE user_id = '.$this->user->user_id.' AND user_id_followed = '.$user_id_followed;
+    	DB::instance(DB_NAME)->delete('users_users', $where_condition);
+
+   		//Send them back
+    	Router::redirect("/posts/users");
+
+	}
 
 	 public function index(){
 	 	//Set up the view
--- js/posts_add.js
+++ js/posts_add.js
@@ -1,18 +1,14 @@
 // Set up the options for ajax
- var options = {
-                    
-    type: 'post',
-    url: 'posts/p_add/',
-
+var options = { 
+    type: 'POST',
+    url: '/posts/p_add/',
     beforeSubmit: function() {
         $('#results').html("Adding...");
     },
-
-    //Hide form and display results
-    success: function(response) {
-        $(".madlib_form").hide();
+    success: function(response) {   
         $('#results').html(response);
-	}
-};
+    } 
+}; 
 
+// Using the above options, ajax'ify the form
 $('form').ajaxForm(options);
