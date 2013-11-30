cff906ba9553b398310cf9f8841bdc011d833cf7
ann adelsberger <adelsbergerann@gmail.com>
2013-11-29 22:39:18 -0500

    add bootstrap.js

--- controllers/c_posts.php
+++ controllers/c_posts.php
@@ -12,7 +12,8 @@ class posts_controller extends base_controller{
 	    $client_files_body = Array(
 	        "/js/jquery.form.js",
 	        "/js/posts_add.js", 
-	        "/js/respond.js"
+	        "/js/respond.js", 
+	        "/js/bootstrap.js"
 	    );
 
 	    $this->template->client_files_body = Utils::load_client_files($client_files_body);   
