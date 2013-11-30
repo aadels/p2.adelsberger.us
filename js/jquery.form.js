6e5a175d355787a70942da6339ee015c368794f5
ann adelsberger <adelsbergerann@gmail.com>
2013-11-29 22:44:12 -0500

    remove form sizing line

--- js/jquery.form.js
+++ js/jquery.form.js
@@ -1203,6 +1203,3 @@ function log() {
 
 })( (typeof(jQuery) != 'undefined') ? jQuery : window.Zepto );
 
-window.addEvent('domready', function() {
-    $('form-fieldset').setStyle('height', $('form-fieldsett').getStyle('height').toInt() - 20);
-});
