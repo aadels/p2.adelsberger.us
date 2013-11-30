eb4d55f5dc311797005216b22fcef32f1e13e446
ann adelsberger <adelsbergerann@gmail.com>
2013-11-29 19:58:53 -0500

    delete redundany js files add madlic styles

--- views/_v_template.php
+++ views/_v_template.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title><?php if(isset($title)) echo $title . " - "; ?>YaketyYak</title>
+		<title>Madlibs!</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	    
 	    <link href="/css/styles.css" rel="stylesheet">
+	    <link href="/css/madlib.css" rel="stylesheet">
 	    
 	    <!-- Bootstrap -->
 	    <link href="/css/bootstrap.min.css" rel="stylesheet">
