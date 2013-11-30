bed007d0920e981a53b3ed27cdf020749580e37b
ann adelsberger <adelsbergerann@gmail.com>
2013-11-29 20:15:04 -0500

    added styles

--- views/_v_template.php
+++ views/_v_template.php
@@ -6,10 +6,11 @@
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	    
 	    <link href="/css/styles.css" rel="stylesheet">
+	    <link href="/css/madlib.css" rel="stylesheet">
 	    
 	    <!-- Bootstrap -->
 	    <link href="/css/bootstrap.min.css" rel="stylesheet">
-
+	    <link href="/css/bootstrap-theme.min" rel="stylesheet">
 	    <!-- fonts -->
 
 						
