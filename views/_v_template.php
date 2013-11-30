ba51b915af7443b23057f9709a97d69b363b598d
ann adelsberger <adelsbergerann@gmail.com>
2013-11-29 20:20:43 -0500

    Revert 3f4b2db..d714caf
    This rolls back to commit 3f4b2db5c2af5f004dbda8b2150c1264328a7cf0.

--- views/_v_template.php
+++ views/_v_template.php
@@ -6,11 +6,10 @@
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	    
 	    <link href="/css/styles.css" rel="stylesheet">
-	  
 	    
 	    <!-- Bootstrap -->
 	    <link href="/css/bootstrap.min.css" rel="stylesheet">
-	    
+
 	    <!-- fonts -->
 
 						
