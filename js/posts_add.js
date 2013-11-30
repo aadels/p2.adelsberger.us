921c7572c5a84353d2246ceb8bd598ff39bdc563
ann adelsberger <adelsbergerann@gmail.com>
2013-11-29 20:31:59 -0500

    adited url on posst_add.js

--- js/posts_add.js
+++ js/posts_add.js
@@ -2,7 +2,7 @@
  var options = {
                     
     type: 'post',
-    url: '/posts/posts_index/',
+    url: '/posts/p_add/',
     beforeSend: function() {   
     //Display a loading message while waiting for the ajax call to complete
         $('#results').html("Loading...");
