257021de27b96d131770ac12b5fedb215432c72e
ann adelsberger <adelsbergerann@gmail.com>
2013-11-29 22:07:11 -0500

    add style link

--- views/_v_template.php
+++ views/_v_template.php
@@ -6,6 +6,7 @@
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	    
 	    <link href="/css/styles.css" rel="stylesheet">
+	    <link href="/css/madlib.css" rel="stylesheet">
 	    
 	    <!-- Bootstrap -->
 	    <link href="/css/bootstrap.min.css" rel="stylesheet">
