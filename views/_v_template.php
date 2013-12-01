5dcd255f963b97c8cbe13de50424d9d1f62c6810
ann adelsberger <adelsbergerann@gmail.com>
2013-12-01 09:36:15 -0500

    roll back

--- views/_v_template.php
+++ views/_v_template.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Madlibs!</title>
+		<title><?php if(isset($title)) echo $title . " - "; ?>YaketyYak</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	    
@@ -10,6 +10,9 @@
 	    <!-- Bootstrap -->
 	    <link href="/css/bootstrap.min.css" rel="stylesheet">
 
+	    <!-- fonts -->
+
+						
 		<!-- Controller Specific JS/CSS -->
 		<?php if(isset($client_files_head)) echo $client_files_head; ?>
 		<!--custom css-->
@@ -60,11 +63,17 @@
             <div class="container">
                 <div class="row">
                     <div class="col-xs-6">
-                        <p>Madlibs! </p>
+                        <p>YaketyYak<br>
+                           +1 features:<br>
+                        </p>
+                        <ul>
+                            <li>Emailing a welcome letter to new users.</li>
+                            <li>Uploading/changing profile picture</li>
+                        </ul>
                     </div>
                     <div class="col-xs-6">
                         <p class="right pull-right">Ann Adelsberger<br>
-                        	CSCI E-15 Project 3<br>
+                        	CSCI E-15 Project 2<br>
                         	Fall 2013<br>
                             <a href="mailto:adelsbergerann@gmail.com">adelsbergerann@gmail.com</a>
                         </p>
