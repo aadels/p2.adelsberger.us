ffd0f9877a1d182c2abf788164587343263b78ad
ann adelsberger <adelsbergerann@gmail.com>
2013-12-01 10:05:23 -0500

    change view

--- controllers/c_posts.php
+++ controllers/c_posts.php
@@ -6,14 +6,14 @@ class posts_controller extends base_controller{
 
 	    # Setup view
 	    $this->template->content = View::instance('v_posts_add');
-	    $this->template->title   = "Add a new post";
+	    $this->template->title   = "Madlib 1";
 
 	    # Load JS files
 	    $client_files_body = Array(
 	        "/js/jquery.form.js",
 	        "/js/posts_add.js", 
 	        "/js/respond.js", 
-	       
+	        "/js/bootstrap.js"
 	    );
 
 	    $this->template->client_files_body = Utils::load_client_files($client_files_body);   
@@ -23,9 +23,7 @@ class posts_controller extends base_controller{
 	}
 		
 	public function p_add(){
-
 		Router::redirect('/views/v_posts_index.php'); 
-
 	}
 	
 
