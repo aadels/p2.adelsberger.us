8f4c43539ef49ef28cabc690b44e445f67ce9551
ann adelsberger <adelsbergerann@gmail.com>
2013-12-01 13:36:25 -0500

    testing github connection

--- controllers/c_index.php
+++ controllers/c_index.php
@@ -22,6 +22,7 @@ class index_controller extends base_controller {
 			$this->template->title = "Welcome";
 	
 		# CSS/JS includes
+			
 			/*
 			$client_files_head = Array("");
 	    	$this->template->client_files_head = Utils::load_client_files($client_files);
